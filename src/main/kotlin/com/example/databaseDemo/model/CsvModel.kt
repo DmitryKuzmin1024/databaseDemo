a2ed142498af0059677f102a9f4df89048e2dfb3
kuzya <infinitusconscientia@gmail.com>
2021-07-05 23:13:57 +0300

    First commit

--- src/main/kotlin/com/example/databaseDemo/model/CsvModel.kt
+++ src/main/kotlin/com/example/databaseDemo/model/CsvModel.kt
@@ -1,17 +1,10 @@
 package com.example.databaseDemo.model
 
 import com.example.databaseDemo.NoArgConstructor
-import com.opencsv.bean.CsvBindAndJoinByPosition
 import com.opencsv.bean.CsvBindByName
-import com.opencsv.bean.CsvBindByPosition
-import javax.persistence.Entity
-import javax.persistence.Id
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
 
-@Entity
 @NoArgConstructor
-class Customer(
+data class CsvModel(
     @CsvBindByName(column = "year")
     val year: String,
     @CsvBindByName(column = "industry_code_ANZSIC")
@@ -26,6 +19,4 @@ class Customer(
     val value: String,
     @CsvBindByName(column = "unit")
     val unit: String,
-    @Id @GeneratedValue(strategy = GenerationType.AUTO)
-    val id: Long = -1) {
-}
+)
